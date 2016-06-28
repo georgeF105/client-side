cac4ac259de588a49f4e2c93b93608aba9117321
George Francis <georgefrancis105@gmail.com>
2016-06-28 15:09:24 +1200

    Inital commit

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var spaceInfo = require('./views/space-info.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
+document.getElementById('refresh-button').addEventListener("click", refreshInfo)
 
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+function refreshInfo() {
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
 
-  // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
-})
+    // In case you're curious
+    console.log(JSON.parse(data.body)) // FYI: data.body is a string
+
+    // Replace 'Space' below with the response
+    // var target = document.getElementsByTagName('main')[0]
+    var pageTitle = document.getElementById("page-title")
+    console.log('page title', pageTitle)
+    var pageInfo = document.getElementById('page-data')
+    pageTitle.innerHTML = greeting({name: 'Space'})
+    pageInfo.innerHTML = spaceInfo(JSON.parse(data.body))
+
+
+  })
+}
+
+refreshInfo()
